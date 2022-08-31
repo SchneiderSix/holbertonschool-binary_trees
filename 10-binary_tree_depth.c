5830478d60a90386177280ab5a0db3d02026f6ae
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-31 17:15:15 -0300

    Task ten

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,7 +7,6 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	/*size_t leftDepth = 0, rightDepth = 0;*/
 	size_t p = 0;
 
 	if (!tree)
@@ -16,12 +15,4 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 	p = binary_tree_depth(tree->parent);
 	return (p + 1);
-
-	/*leftDepth = binary_tree_depth(tree->left);
-	rightDepth = binary_tree_depth(tree->right);
-
-	if (leftDepth > rightDepth)
-		return (leftDepth + 1);
-	else
-		return (rightDepth + 1);*/
 }
