2310098b62eff51b16f9e99b4c475ff0965e8370
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 14:39:30 -0300

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_depth - find depth of a node in bi tree
+* @tree: node
+* Return: 0 or depth
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t p = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->parent)
+		return (0);
+	p = binary_tree_depth(tree->parent);
+	return (p + 1);
+}
+
+/**
 * binary_tree_is_perfect - check if tree is perfect
 * @tree: root node;
 * Return: 0 not p 1 yes p
