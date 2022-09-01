af0111c6eff4040c7665116710ea61c1863e9077
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 17:41:47 -0300

    Task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,9 +9,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if (parent->left == node)
-		return (parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
 	else
-		return (parent->left);
+		return (node->parent->left);
 	return (NULL);
 }
