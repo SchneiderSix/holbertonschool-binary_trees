c466ffe7e206d7a1cb8189f5d2f4717bb67e8b1d
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-31 21:40:24 -0300

    Task thirteen

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_nodes - count nodes with at least 1 child
+* @tree: root node
+* Return: 0 or count of nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+		return (1);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
