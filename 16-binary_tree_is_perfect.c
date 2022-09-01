6ecdbe1b6b23cf7b592119ad76177d937947dd30
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 14:38:26 -0300

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,11 +16,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree->parent &&
 	!tree->left && !tree->right)
 		return (1);
-	if (tree->left && tree->right)
+	if (binary_tree_depth(tree->left) ==
+	binary_tree_depth(tree->right))
 		flag = 1;
 	if (flag == 0)
 		return (0);
-	if (tree->parent && (!tree->left || !tree->right))
+	if (!tree->left || !tree->right)
 		flag = 0;
 	return (binary_tree_is_perfect(tree->left) &&
 	binary_tree_is_perfect(tree->right));
