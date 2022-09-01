ecbb65bd1a7e83708b247e27e7184f1f6863d842
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 15:59:17 -0300

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,20 +1,25 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_depth - find depth of a node in bi tree
+* depth - find depth of a node in bi tree
 * @tree: node
 * Return: 0 or depth
 */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t depth(const binary_tree_t *tree)
 {
-	size_t p = 0;
+	int ld, rd;
 
 	if (!tree)
 		return (0);
-	if (!tree->parent)
-		return (0);
-	p = binary_tree_depth(tree->parent);
-	return (p + 1);
+	else
+	{
+		ld = depth(tree->left);
+		rd = depth(tree->right);
+		if (ld > rd)
+			return (ld + 1);
+		else
+			return (rd + 1);
+	}
 }
 
 /**
@@ -33,16 +38,16 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	if (tree->left && tree->right)
 	{
-		if (binary_tree_depth(tree->left) ==
-		binary_tree_depth(tree->right)
+		if (depth(tree->left) ==
+		depth(tree->right)
 		/*&& !tree->left->left && !tree->left->right
 		&& !tree->right->left && !tree->right->right*/)
 		{
 			flag = 1;
 			return (1);
 		}
-		if (binary_tree_depth(tree->left) !=
-		binary_tree_depth(tree->right))
+		if (depth(tree->left) !=
+		depth(tree->right))
 		{
 			flag = 0;
 		}
