1741a11aaf3816affed13b53599aa566632adafb
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 13:54:22 -0300

    Task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_is_perfect - check if tree is perfect
+* @tree: root node;
+* Return: 0 not p 1 yes p
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int flag;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		flag = 1;
+		return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
+	if (!tree->left || !tree->right)
+		flag = 0;
+	if (flag == 1)
+		return (1);
+	return (0);
+}
