4c1db7fdfa87f1a846c646d51b4a82b9e66861c7
joakG16 <4379@holbertonschool.com>
2022-09-01 07:55:25 -0700

    function border case added

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,9 +12,10 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL)
 		return;
 
-	func(tree->n); /* print node value first, passing
-			* value from node as paramete
-			*/
+	if (func)
+		func(tree->n); /* print node value first, passing
+				* value from node as paramete
+				*/
 	binary_tree_preorder(tree->left, (*func));
 	binary_tree_preorder(tree->right, (*func));
 }
