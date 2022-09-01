f646d8d9ebfeabde4de5def635c6a7fee014ca6b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-31 21:17:10 -0300

    Betty for eleven

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,11 +4,11 @@
 * binary_tree_size - return size fo tree
 * @tree: root node
 * Return: 0 or size
-*/ 
+*/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	else 
+	else
 		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
