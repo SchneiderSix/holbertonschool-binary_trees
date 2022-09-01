9ce2084f6327f128bba0873a64c101fcc0de7596
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 00:43:42 -0300

    Task fourteen

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,17 +13,19 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (0);
-	if (tree->left && !tree->parent && tree->right)
+	while (!tree->parent)
+	{
+		if (tree->left && tree->right)
 		{
 			i++;
 			q++;
-		}
-	if (tree->left && !tree->parent)
-		i++;
+		} else if (tree->left)
+			i++;
+		else if (tree->right)
+			q++;
+	}
 	if (tree->left)
 		i++;
-	if (tree->right && !tree->parent)
-		q++;
 	if (tree->right)
 		q++;
 	return (binary_tree_balance(tree->left) +
