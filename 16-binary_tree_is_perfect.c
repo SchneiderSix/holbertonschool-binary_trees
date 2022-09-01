262e238e211aa21d93e40e1129b058c62bd95c7f
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 16:42:31 -0300

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,49 +1,35 @@
 #include "binary_trees.h"
 
 /**
-* depth - find depth of a node in bi tree
-* @tree: node
-* Return: 0 or depth
+* binary_tree_is_full - check if tree is full
+* @tree: root node
+* Return: 0 for no or 1 for yes
 */
-size_t depth(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int ld, rd;
-
 	if (!tree)
 		return (0);
-	else
-	{
-		ld = depth(tree->left);
-		rd = depth(tree->right);
-		if (ld > rd)
-			return (ld + 1);
-		else
-			return (rd + 1);
-	}
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+		binary_tree_is_full(tree->right));
+	return (0);
 }
 
 /**
- *binary_tree_height - a function that measures the height of a binary tree
- *@tree: Pointer to the node to measures the height
- *Return: the height of the tree
- *If tree is NULL, your function must return 0
- */
-size_t binary_tree_height(const binary_tree_t *tree)
+* binary_tree_size - return size fo tree
+* @tree: root node
+* Return: 0 or size
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
-	/* check and count first in left edge from root node of subtree */
-	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-
-	/* now in right edge from root node of subtree */
-	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	/* see whose is the bigest to be returned */
-	return (height_left > height_right ? height_left : height_right);
+	else
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
+
 
 /**
 * binary_tree_is_perfect - check if tree is perfect
@@ -56,35 +42,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (!tree->parent &&
-	!tree->left && !tree->right)
-		return (1);
-
-	if (binary_tree_height(tree) == 2 ||
-	binary_tree_height(tree) == 3)
-	{
-		if (tree->left->left && tree->left->right
-		&& tree->right->left && tree->right->right)
+	if (binary_tree_is_full(tree) == 1)
+		if (binary_tree_size(tree->left) - binary_tree_size(tree->right) == 0)
 			return (1);
-	}
-	if (tree->left && tree->right)
-	{
-		if (depth(tree->left) ==
-		depth(tree->right)
-		&& !tree->left->left && !tree->left->right
-		&& !tree->right->left && !tree->right->right)
-		{
-			flag = 1;
-			return (1);
-		}
-	}
-	if (depth(tree->left) == depth(tree->right)
-	&& tree->left->left && tree->left->right
-	&& tree->right->left && tree->right->right)
-		flag = 0;
-
-	if (flag == 0)
-		return (0);
-	return (binary_tree_is_perfect(tree->left) &&
-	binary_tree_is_perfect(tree->right));
+	return (0);
 }
