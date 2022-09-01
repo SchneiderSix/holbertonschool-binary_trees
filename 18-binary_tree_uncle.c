a14cc3b2858c62f1549a84252f0d98474b4af426
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-01 17:54:58 -0300

    Task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_sibling - search your brother
+* @node: search brother of node
+* Return: null or node
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+	return (NULL);
+}
+
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
